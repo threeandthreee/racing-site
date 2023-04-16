7914afc8076995ebcf3e15b2f20c191019dfa7a3
Alex Nordstrom <a.l.nordstrom@gmail.com>
2023-04-15 20:28:22 -0400

    update bingo link

--- src/views/BingoView.vue
+++ src/views/BingoView.vue
@@ -8,13 +8,13 @@ v-app
         v-btn.pa-0(text disabled x-large) Bingo
       p Play bingo while you watch the race! If these squares don't make sense to you, don't worry -- we'll call them out as they happen. Or ask in chat!
       v-card.mb-4(outlined)
-        iframe(width="100%" frameborder="0" style="aspect-ratio:1" src="https://bingobaker.com/#5842828")
+        iframe(width="100%" frameborder="0" style="aspect-ratio:1" src="https://bingobaker.com/#643b314c12992b85")
       v-switch(label="Show Stream" v-model="showVideo")
       v-card.mb-4(outlined v-if="showVideo")
         iframe(src="https://player.twitch.tv/?channel=raceswild&parent=raceswild.3and3.dev&parent=racing.3and3.dev&parent=raceswild.com&parent=localhost" frameborder="0" allowfullscreen="true" scrolling="no"  width="100%" style="aspect-ratio: 16/9")
       v-switch(label="Show Chat" v-model="showChat")
       v-card.mb-4(outlined v-if="showChat")
-        iframe(src="https://www.twitch.tv/embed/raceswild/chat?parent=raceswild.3and3.dev&parent=racing.3and3.dev&parent=raceswild.com&parent=localhost" frameborder="0" allowfullscreen="true" scrolling="no"  width="100%" height="600px")
+        iframe(src="https://www.twitch.tv/embed/raceswild/chat?parent=raceswild.3and3.dev&parent=racing.3and3.dev&parent=raceswild.com&parent=localhost" frameborder="0" allowfullscreen="true" scrolling="no"  width="100%" height="300px")
 </template>
 
 <script>
