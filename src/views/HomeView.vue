e65affb612e066a6bd28ee1fb290b198e1223b92
Alex Nordstrom <a.l.nordstrom@gmail.com>
2023-07-26 01:26:15 -0400

    redo home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,26 +5,31 @@ v-app
       .overline Next Up
       .text-h4.text-sm-h3.text-md-h2.text-xl-h1 {{display.title}}
       .text-h6.text-sm-h5.text-md-h4.text-xl-h3 {{display.date}}
-      .text-body1.my-4 We run speedrun races every other week!
-      div.mx-n1.mb-3.mt-n1.d-flex.flex-wrap
-        v-btn.ma-1(color="primary" href="https://discord.gg/V7caHdC8r3" target="_blank") Aces Wild Discord
-        v-btn.ma-1(color="primary" :href="calendarUrl" target="_blank") Race Calendar
-        v-btn.ma-1(color="primary" to="/bingo" target="_blank") Viewer Bingo!
-      .vid.mx-n3.mx-md-0
-        div(v-if="isRacingNow")
-          iframe(src="https://player.twitch.tv/?channel=raceswild&parent=raceswild.3and3.dev&parent=racing.3and3.dev&parent=raceswild.com&parent=localhost" frameborder="0" allowfullscreen="true" scrolling="no"  width="100%" style="aspect-ratio: 16/9")
-        div(v-if="!isRacingNow")
-          iframe(src="https://player.twitch.tv/?collection=_5tpY1b8KReJxw&parent=raceswild.3and3.dev&parent=racing.3and3.dev&parent=raceswild.com&parent=localhost&muted=true&autoplay=true" frameborder="0" allowfullscreen="true" scrolling="no" width="100%" style="aspect-ratio: 16/9")
-    #history.mt-4.fade-in(v-if="history.length")
+      .text-body1.my-4 Speedrun races every other week!
+      div.mx-n1.mb-3.mt-n1.d-flex.flex-wrap.align-center
+        v-btn(tile icon x-large href="https://twitch.tv/raceswild" target="_blank").pa-2
+          img(src="TwitchGlitchPurple.svg")
+        v-btn(tile icon x-large href="https://discord.gg/V7caHdC8r3" target="_blank").pa-1
+          img(src="discord-mark-blue.svg")
+        v-btn(tile icon x-large :href="calendarUrl" target="_blank").pa-2
+          img(src="Google_Calendar_icon_(2020).svg")
+      v-btn(color="primary" outlined to="/bingo") Viewer Bingo
+    #history.mt-8.fade-in(v-if="history.length")
       .overline Race History
-      v-row
-        v-col(cols=12 v-for="race in history" :key="race.id")
-          v-card(outlined)
-            v-card-title(style="word-break: normal")
-              .d-flex.justify-space-between.w-100
-                span {{race.summary}}
-                v-chip.ml-2.flex-shrink-0(color="primary") {{race.date}}
-            v-card-text(v-html="race.description")
+      v-data-iterator(
+        :items="history"
+        item-key="id"
+        :items-per-page="5"
+      )
+        template(v-slot:default="{ items }")
+          v-row.ma-0.pa-0
+            v-col.px-0.py-1(cols=12 v-for="race in items" :key="race.id")
+              v-card(outlined)
+                v-card-title(style="word-break: normal").pb-1
+                  .d-flex.justify-space-between.w-100
+                    span {{race.summary}}
+                    v-chip.ml-2.flex-shrink-0(color="primary") {{race.date}}
+                v-card-text(v-html="race.description")
 </template>
 
 <script>
